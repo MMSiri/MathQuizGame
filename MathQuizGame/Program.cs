1990ec6ea3ce8e0d3204d2b58840a07cb6b9e53e
Mika Mirzanasiri <mika-mm@hotmail.com>
2022-07-20 19:57:00 +0100

    Add project files.

---
+++ MathQuizGame/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MathQuizGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Q q = new Q(Q.R.Next(2) == 1);
+            while (true)
+            {
+                Console.Write($"{q.N1} {q.Op} {q.N2} = ");
+                if (!int.TryParse(Console.ReadLine(), out int i))
+                {
+                    Console.WriteLine("Thanks for playing!");
+                    return;
+                }
+                if (q.Check(i))
+                {
+                    Console.WriteLine("Correct!");
+                    q = new Q(Q.R.Next(2) == 1);
+                }
+                else Console.WriteLine("Incorrect. Try again!");
+            }
+
+        }
+    }
+}
